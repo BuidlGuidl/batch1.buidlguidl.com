818ec1bd6b48039f032c479ab5b21d5668b88973
P A P A  W H E E L I E <86974690+mrpapawheelie@users.noreply.github.com>
2024-01-16 11:28:21 -0700

    Updated to use appchains and removed the Image component since the src did not like the fact ens did not have the image filetype in the url.

--- packages/nextjs/pages/builders/0x2F142f9F52af0845B7b8b1353E349Ca1Cb8EFE92.tsx
+++ packages/nextjs/pages/builders/0x2F142f9F52af0845B7b8b1353E349Ca1Cb8EFE92.tsx
@@ -1,20 +1,16 @@
 import { useEffect, useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
-import { createPublicClient, http } from "viem";
-import { mainnet } from "viem/chains";
+import { appChains } from "~~/services/web3/wagmiConnectors";
 
-export function PapaWheelie() {
+export default function PapaWheelie() {
   const [ensData, setEnsData] = useState<Record<string, string | undefined>>({});
   const nameTitle = "P A P A W H E E L I E";
   const ensName = "papawheelie.eth";
 
   useEffect(() => {
-    const client = createPublicClient({
-      chain: mainnet,
-      transport: http(),
+    const client = appChains.publicClient({
+      chainId: 1,
     });
-
     async function fetchEnsTexts() {
       const keys = ["com.twitter", "bio", "com.discord", "com.youtube", "com.github", "avatar"];
       try {
@@ -44,46 +40,50 @@ export function PapaWheelie() {
   }, []);
 
   return (
-    <div className="mx-auto max-w-3xl p-0 shadow-2xl">
-      <div className="flex flex-col md:flex-row items-center md:items-stretch space-y-4 md:space-y-0 md:space-x-4">
-        <div className="md:w-1/3 flex justify-center md:justify-start">
-          <Image
-            className="rounded-l-md md:rounded-r-none"
-            src={ensData["avatar"] ?? "Loading..."}
-            alt={ensData["name"] ?? "Loading..."}
-          />
-        </div>
-        <div className="md:w-2/3 flex flex-col justify-between p-4">
-          <div>
-            <p className="text-lg antialiased my-0 align-text-top leading-normal">{nameTitle}</p>
-            <span className="text-sm text-gray-500 dark:text-gray-400">{ensName}</span>
-            <p className="text-sm my-2 text-center md:text-left">{ensData["bio"] ?? "Loading..."}</p>
+    <div className="flex h-lvh flex-grow items-center justify-center px-4">
+      <div className="mx-auto max-w-3xl rounded-md shadow-2xl shadow-indigo-500/40 md:shadow-indigo-500/40 md:shadow-2xl">
+        <div className="flex flex-col md:flex-row items-center items-stretch justify-center gap-4">
+          <div className="md:w-1/3 flex justify-center md:justify-start">
+            <img
+              className="rounded-full w-1/2 h-1/2 md:h-full md:w-full md:object-cover md:rounded-l-md md:rounded-r-none"
+              src={ensData["avatar"] ?? "https://i.imgur.com/UhV7H97.jpeg"}
+              alt={ensData["name"] ?? "Loading..."}
+            />
           </div>
-          <div className="flex flex-wrap justify-between items-end">
-            <Link
-              href={ensData["com.twitter"] ?? "#"}
-              className="bg-blue-400 hover:bg-blue-400/50 text-white p-2 rounded"
-            >
-              Twitter
-            </Link>
-            <Link
-              href={ensData["com.github"] ?? "Loading..."}
-              className="bg-[#000] hover:bg-[#000]/50 text-white p-2 rounded"
-            >
-              Github
-            </Link>
-            <Link
-              href={ensData["com.youtube"] ?? "Loading..."}
-              className="bg-[#FF0000] hover:bg-[#ff0000]/50 text-white p-2 rounded"
-            >
-              Youtube
-            </Link>
-            <Link
-              href={ensData["com.discord"] ?? "Loading..."}
-              className="bg-[#5865F2] hover:bg-[#5865F2]/50 text-white p-2 rounded"
-            >
-              Discord
-            </Link>
+          <div className="md:w-2/3 flex flex-col justify-between p-8">
+            <div className="text-center md:text-start divide-y divide-solid divide-slate-500/30">
+              <p className="my-0">{nameTitle}</p>
+              <span className="text-sm text-gray-500 dark:text-gray-400">{ensName}</span>
+            </div>
+            <div className="text-center md:text-left pb-2">
+              <p>{ensData["bio"] ?? "Loading..."}</p>
+            </div>
+            <div className="flex flex-wrap justify-between items-end">
+              <Link
+                href={ensData["com.twitter"] ?? "#"}
+                className="bg-blue-400 hover:bg-blue-400/50 text-white p-2 rounded"
+              >
+                Twitter
+              </Link>
+              <Link
+                href={ensData["com.github"] ?? "Loading..."}
+                className="bg-[#000] hover:bg-[#000]/50 text-white p-2 rounded"
+              >
+                Github
+              </Link>
+              <Link
+                href={ensData["com.youtube"] ?? "Loading..."}
+                className="bg-[#FF0000] hover:bg-[#ff0000]/50 text-white p-2 rounded"
+              >
+                Youtube
+              </Link>
+              <Link
+                href={ensData["com.discord"] ?? "Loading..."}
+                className="bg-[#5865F2] hover:bg-[#5865F2]/50 text-white p-2 rounded"
+              >
+                Discord
+              </Link>
+            </div>
           </div>
         </div>
       </div>
