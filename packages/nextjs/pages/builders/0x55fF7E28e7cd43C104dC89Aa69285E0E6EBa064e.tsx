78ed168896521c87515e94a4d21ec6690e1547b9
Yassirakh <40870357+Yassirakh@users.noreply.github.com>
2024-01-17 18:07:23 +0100

    added yassirakh.eth builder page (#13)

---
+++ packages/nextjs/pages/builders/0x55fF7E28e7cd43C104dC89Aa69285E0E6EBa064e.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+const YassirakhBuilderPage = () => {
+  return (
+    <div className="flex flex-col items-center w-100 mt-4 text-center">
+      <Image className="rounded-3xl flex" src="/yassirakh.jpg" alt="yassirakh.eth image" width={200} height={200} />
+      <div className="flex flex-col mt-5">
+        <span>
+          Hello, my name is Yassir (yassirakh.eth), I&apos;m a web2 dev, I&apos;ve deving mainly in PHP using Symfony
+          and Drupal, and currently I&apos;m trying to build cool stuff on the web3.
+        </span>
+        <div className="flex flex-col">
+          <span>
+            Github :{" "}
+            <a className="text-blue-600" href="https://github.com/Yassirakh" target="_blank" rel="noreferrer">
+              link
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default YassirakhBuilderPage;
