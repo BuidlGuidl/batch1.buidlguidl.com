7e05ef924643aa2a2c5e2f03f08ea3a2f48d58f4
unknown <akhmisy@gmail.com>
2024-01-18 19:13:27 +0100

    Made changes requested in issue #4*

--- packages/nextjs/components/batch1/AccountInfo.tsx
+++ packages/nextjs/components/batch1/AccountInfo.tsx
@@ -23,30 +23,28 @@ const AccountInfo = () => {
 
   return (
     <>
-      {userAllowlistData !== undefined && userContractAddressData !== undefined && (
-        <div className="py-2 px-5 ml-4 rounded-2xl text-sm font-semibold bg-[#DAE8FF]">
-          <div>
-            <div className="flex flex-row content-center">
-              AllowListed{" "}
-              {userAllowlistData ? (
-                <CheckBadgeIcon className="ml-1" color="green" width={20} />
-              ) : (
-                <XMarkIcon className="ml-1" color="red" width={20} />
-              )}
-            </div>
-          </div>
-          <div>
-            <div className="flex flex-row content-center">
-              Checked in{" "}
-              {userContractAddressData != "0x0000000000000000000000000000000000000000" ? (
-                <CheckBadgeIcon className="ml-1" color="green" width={20} />
-              ) : (
-                <XMarkIcon className="ml-1" color="red" width={20} />
-              )}
-            </div>
+      <div className="py-2 px-5 ml-4 rounded-2xl text-sm font-semibold bg-[#DAE8FF]">
+        <div>
+          <div className="flex flex-row content-center">
+            AllowListed{" "}
+            {userAllowlistData ? (
+              <CheckBadgeIcon className="ml-1" color="green" width={20} />
+            ) : (
+              <XMarkIcon className="ml-1" color="red" width={20} />
+            )}
           </div>
         </div>
-      )}
+        <div>
+          <div className="flex flex-row content-center">
+            Checked in{" "}
+            {userContractAddressData != "0x0000000000000000000000000000000000000000" ? (
+              <CheckBadgeIcon className="ml-1" color="green" width={20} />
+            ) : (
+              <XMarkIcon className="ml-1" color="red" width={20} />
+            )}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
