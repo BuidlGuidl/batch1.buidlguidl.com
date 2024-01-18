7936b2d79b9c6b11a73637a7787380935c70f749
unknown <akhmisy@gmail.com>
2024-01-18 19:11:34 +0100

    Made changes requested in issue #4*

--- packages/nextjs/components/batch1/AccountInfo.tsx
+++ packages/nextjs/components/batch1/AccountInfo.tsx
@@ -17,31 +17,33 @@ const AccountInfo = () => {
     args: [accountState.address],
   });
 
+  if (!accountState.address) {
+    return null;
+  }
+
   return (
     <>
       {userAllowlistData !== undefined && userContractAddressData !== undefined && (
         <div className="py-2 px-5 ml-4 rounded-2xl text-sm font-semibold bg-[#DAE8FF]">
           <div>
-            {userAllowlistData == true ? (
-              <div className="flex flex-row content-center">
-                AllowListed <CheckBadgeIcon className="ml-1" color="green" width={20} />
-              </div>
-            ) : (
-              <div className="flex flex-row content-center">
-                AllowListed <XMarkIcon className="ml-1" color="red" width={20} />
-              </div>
-            )}
+            <div className="flex flex-row content-center">
+              AllowListed{" "}
+              {userAllowlistData ? (
+                <CheckBadgeIcon className="ml-1" color="green" width={20} />
+              ) : (
+                <XMarkIcon className="ml-1" color="red" width={20} />
+              )}
+            </div>
           </div>
           <div>
-            {userContractAddressData != "0x0000000000000000000000000000000000000000" ? (
-              <div className="flex flex-row content-center">
-                Checked in <CheckBadgeIcon className="ml-1" color="green" width={20} />
-              </div>
-            ) : (
-              <div className="flex flex-row content-center">
-                Checked in <XMarkIcon className="ml-1" color="red" width={20} />
-              </div>
-            )}
+            <div className="flex flex-row content-center">
+              Checked in{" "}
+              {userContractAddressData != "0x0000000000000000000000000000000000000000" ? (
+                <CheckBadgeIcon className="ml-1" color="green" width={20} />
+              ) : (
+                <XMarkIcon className="ml-1" color="red" width={20} />
+              )}
+            </div>
           </div>
         </div>
       )}
